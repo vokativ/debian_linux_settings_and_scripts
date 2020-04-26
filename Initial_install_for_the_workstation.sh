dc3623ea823088e1cd2fff86705bcab3dbf81c17
NemanjaG <32364362+vokativ@users.noreply.github.com>
2020-04-26 21:32:15 +0800

    Changes to be committed: 	modified:   Initial_install_for_the_workstation.sh to add remianing packages and reduce the answers on apt install

--- Initial_install_for_the_workstation.sh
+++ Initial_install_for_the_workstation.sh
@@ -2,56 +2,95 @@
 #!/bin/bash
 
 #add missing repositories
+echo "Направи кафу"
 echo 'Adding repositories...'
 
 #update system
-echo 'Updating current install...'
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+    echo 'Updating current install...'
+    sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 
 #now install my usual packages
-echo 'installing bassic software and flathub...'
-sudo apt install firefox htop chromium-browser chromium-codecs-ffmpeg-extra vlc cheese flatpak gnome-software-plugin-flatpak git
+    echo 'installing basic software and flathub. This will take a while'
+    sudo DEBIAN_FRONTEND=noninteractive apt install --yes firefox htop chromium-browser chromium-codecs-ffmpeg-extra vlc cheese neofetch curl python ruby flatpak gnome-software-plugin-flatpak git
 
 #installing vivaldi
 echo 'Installing Vivaldi. If breaks, go to https://vivaldi.com/download/'
 
-wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
-sudo apt update && sudo apt install vivaldi-stable
+    wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
+    sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main'
+    sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install --yes vivaldi-stable
 
 #setup my usual flatpak apps
 echo 'Installing Flatpak apps...'
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.viber.Viber
-flatpak install flathub com.skype.Client
-flatpak install flathub org.libreoffice.LibreOffice
-flatpak install flathub org.telegram.desktop
-flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub com.gitlab.librebob.Athenaeum
-flatpak install flathub com.valvesoftware.Steam
-flatpak install flathub com.github.tchx84.Flatseal
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak install flathub com.viber.Viber
+    flatpak install flathub com.skype.Client
+    flatpak install flathub org.libreoffice.LibreOffice
+    flatpak install flathub org.telegram.desktop
+    flatpak install flathub org.mozilla.Thunderbird
+    flatpak install flathub com.gitlab.librebob.Athenaeum
+    flatpak install flathub com.valvesoftware.Steam
+    flatpak install flathub com.github.tchx84.Flatseal
 
 #install the snaps for work
 echo 'Installing tandem and hub for Github...'
-sudo snap install tandem
-sudo snap install hub --classic
+    sudo snap install tandem
+    sudo snap install hub --classic
 
 #setup permissions for Tandem
 echo 'Setting up permissions for tandem'
-snap connect tandem:camera :camera
-snap connect tandem:audio-record :audio-record
-snap connect tandem:pulseaudio :pulseaudio
+    snap connect tandem:camera :camera
+    snap connect tandem:audio-record :audio-record
+    snap connect tandem:pulseaudio :pulseaudio
 
 #Remember to install InSync for Google Drive InSync
-echo 'Download the latest Gogole Drive sync client at https://www.insynchq.com/downloads'
-nohup firefox https://www.insynchq.com/downloads
+    echo 'Download the latest Google Drive sync client at https://www.insynchq.com/downloads'
+    nohup firefox https://www.insynchq.com/downloads
 
 #Install the awesome ICE from Peppermint
 echo 'If you are not on Peppermint, go to Peppermint OS PPAs and download the latest ICE'
-nohup firefox https://launchpad.net/~peppermintos
+    nohup firefox https://launchpad.net/~peppermintos
 
 #setup the Github and GitLab sync with this folder
-echo "To setup sync of this backup folder to Github and GitLab at the same time, follow instructions from https://moox.io/blog/keep-in-sync-git-repos-on-github-gitlab-bitbucket/"
-nohup firefox https://moox.io/blog/keep-in-sync-git-repos-on-github-gitlab-bitbucket/
-#echo "Get ready with your Github and GitLab tokens"
-#git remote set-url origin --add https://gitlab.com/vokativ/debian_linux_settings_and_scripts.git
+
+    #setup the actual git stuff
+
+    gem install gitlab
+    #Create github folder in home and sync the repository
+    mkdir ~/github
+    cd ~/github
+    git clone https://github.com/vokativ/debian_linux_settings_and_scripts.git
+
+    #ask if git credentials are copied
+    echo 'Setting up the git sync for backups'
+    read -p 'Have you copied the git credentials to setup the sync to Github and GitLab? y(es) or n(o): ' copied_git_credentials
+
+    if [copied_git_credentials == 'yes'] || [copied_git_credentials == 'y']
+    then
+        #setup the sync with Github and GitLab
+        export GIT_USER_NAME=vokativ
+        export GIT_REPO_NAME=debian_linux_settings_and_scripts
+
+        #setup the mirrors
+        cd ~/github/${GIT_REPO_NAME}/
+        git remote set-url origin --add https://gitlab.com/${GIT_USER_NAME}/${GIT_REPO_NAME}.git
+        #not working for now git remote set-url origin --add https://bitbucket.org/${GIT_USER_NAME}/${GIT_REPO_NAME}.git
+    else
+        echo "To setup sync of this backup folder to Github and GitLab at the same time, follow instructions from https://moox.io/blog/keep-in-sync-git-repos-on-github-gitlab-bitbucket/"
+        nohup firefox https://moox.io/blog/keep-in-sync-git-repos-on-github-gitlab-bitbucket/
+    fi
+    
+#Background work for GitHub Desktop in Electron
+    # Using Ubuntu
+    curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y nodejs
+
+    #install yarn
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://nightly.yarnpkg.com/debian/ nightly main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install yarn
+
+    #remaining dependencies for electron
+    sudo DEBIAN_FRONTEND=noninteractive apt install libsecret-1-dev libgconf-2-4
+    
+echo 'Готово! Finally done'
